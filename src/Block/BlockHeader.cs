0623401741a52f4fcb7adfe162ac466c3bc398d2
Yong Jian Ming <jianming1993@gmail.com>
2019-08-02 01:14:20 +0800

    Add Block/BlockHeader.cs

---
+++ src/Block/BlockHeader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tuckfirtle.Core.Block
+{
+    public class BlockHeader
+    {
+        public byte Version { get; set; }
+
+        public DateTimeOffset DateTime { get; set; }
+
+        public ulong Nonce { get;set; }
+
+        public ulong DifficultyTarget { get; set; }
+    }
+}
