1e6e00efa418c57398b255142b38dca71778fc73
Yong Jian Ming <jianming1993@gmail.com>
2019-08-05 01:42:07 +0800

    Update constants.

--- src/CoreSettings.cs
+++ src/CoreSettings.cs
@@ -1,4 +1,6 @@
-﻿namespace Tuckfirtle.Core
+﻿using System;
+
+namespace Tuckfirtle.Core
 {
     /// <summary>
     /// Core settings class that contains settings for this blockchain.
@@ -21,6 +23,11 @@
         public const ulong CoinAtomicUnit = 100000000;
 
         /// <summary>
+        /// Coin total supply in atomic unit. (TBD)
+        /// </summary>
+        public const ulong CoinTotalSupply = 10000000000000000000;
+
+        /// <summary>
         /// Current block version.
         /// </summary>
         public const int BlockVersion = 1;
@@ -31,6 +38,16 @@
         public const int GenesisBlockDifficulty = 1000;
 
         /// <summary>
+        /// Current transaction version.
+        /// </summary>
+        public const int TransactionVersion = 1;
+
+        /// <summary>
+        /// Minimum transaction fee needed to make an transaction.
+        /// </summary>
+        public const ulong MinimumTransactionFee = 0;
+
+        /// <summary>
         /// Tuckfirtle pow scratchpad size.
         /// </summary>
         public const int TuckfirtlePowScratchpadSize = 2 * 32 * 1024;
@@ -39,5 +56,20 @@
         /// Tuckfirtle pow memory loop round.
         /// </summary>
         public const int TuckfirtlePowMemoryLoopRound = 1;
+
+        /// <summary>
+        /// Peer to peer (P2P) default port.
+        /// </summary>
+        public const ushort P2PDefaultPort = 0;
+
+        /// <summary>
+        /// Remote procedural call (RPC) default port.
+        /// </summary>
+        public const ushort RPCDefaultPort = 0;
+
+        /// <summary>
+        /// Unique network id for network communication.
+        /// </summary>
+        public static Guid NetworkId = new Guid(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
     }
 }
